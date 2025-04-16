5cada966c20e8f3ee4446b9222af9b8fa1589c01
tkhsksk <tkhsksk0318@gmail.com>
2025-04-16 16:10:13 +0900

    Update PostController.php

--- app/Http/Controllers/Manager/PostController.php
+++ app/Http/Controllers/Manager/PostController.php
@@ -55,8 +55,7 @@ class PostController extends Controller
         $destinationPath = basename($request->image);
 
         // 画像inputがあればファイル移動
-        if ($sourcePath)
-            $moved = Storage::disk('public')->move($sourcePath, $destinationPath);
+        $moved = $sourcePath ? Storage::disk('public')->move($sourcePath, $destinationPath) : '';
 
         if ($moved)
             $files = Storage::disk('public')->files('temp/'.$userId);
