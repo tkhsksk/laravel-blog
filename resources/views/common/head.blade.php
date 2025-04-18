4c7fecd64b792403c704149046bf6de0f9d5664c
tkhsksk <tkhsksk0318@gmail.com>
2025-04-18 16:06:22 +0900

    ogp

--- resources/views/common/head.blade.php
+++ resources/views/common/head.blade.php
@@ -1,5 +1,15 @@
 @section('common.head')
 <meta charset="utf-8">
+@if(Request::routeIs('post.detail'))
+	<meta property="og:url" content="{{ route('post.detail', $post->id) }}"/>
+	<meta property="og:type" content="blog"/>
+	<meta property="og:title" content="{{ $post->title }}"/>
+	<meta property="og:description" content="ポスト記事"/>
+	<meta property="og:site_name" content="{{ config('app.name') }}"/>
+	@isset($post->image)
+		<meta property="og:image" content="{{ route('home') }}{{ $post->image }}"/>
+	@endisset
+@endif
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="shortcut icon" href="@asset('/favicon.png')">
 <link href="@asset('/css/bootstrap.min.css')" rel="stylesheet">
